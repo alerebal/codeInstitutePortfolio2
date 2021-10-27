f01ad05ceb08aa9508977c3e73c6f905454e466f
alerebal <ale.rebal@gmail.com>
2021-10-27 11:00:39 +0100

    Add funtions to the main.js
    Adding the functionality of the game
    who win a small game and who win the final game
    display messages and managing the score

---
+++ assets/js/main.js
@@ -0,0 +1,56 @@
+
+/**
+ * @returns player choice
+ */
+ function playerChoice() {
+
+}
+
+/**
+ * @returns cmp choice
+ */
+function cmpChoice() {
+
+}
+
+/**
+ * @returns determine who wins between the player and the cmp
+ */
+ function whoWins() {
+
+ }
+
+/**
+ * determine who is the winner of the small game
+ */
+ function battle() {
+
+ }
+
+ /**
+ * @returns total points of each player on the game. 
+ */
+function totalPoints() {
+
+}
+
+/**
+ * display the score on the side
+ */
+ function displayScore() {
+
+ }
+
+ /**
+ * display a message on the screen about game situation
+ */
+function displayMessageScore() {
+
+} 
+
+/**
+ * @returns who obtains the final victory
+ */
+ function finalWinner() {
+
+ }
